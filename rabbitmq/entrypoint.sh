4f2e20b6813fac22189b467ab3d70004d98e68c3
Martin Askolin <80485419+martinaskolin@users.noreply.github.com>
2025-02-27 11:27:35 +0100

    fixed rabbitmq cluster issues
    changed eol from ctlf to lf, also changed entrypoint to launch rabbitmq before trying to shut it down which caused the clustring to fail...

--- rabbitmq/entrypoint.sh
+++ rabbitmq/entrypoint.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+# Start RabbitMQ server in the background
+rabbitmq-server &
 
+# Wait for RabbitMQ to be fully up before continuing
+echo "Waiting for RabbitMQ to start..."
+until rabbitmqctl status; do
+  sleep 5
+done
 
 # If this is NOT the first node, try to join the cluster
 if [ "$JOIN_CLUSTER_HOST" != "" ]; then
-  sleep 10
+  echo "Waiting for cluster host to launch..."
+  sleep 5
   echo "Joining RabbitMQ cluster at $JOIN_CLUSTER_HOST..."
   rabbitmqctl stop_app
+  rabbitmqctl reset
   rabbitmqctl join_cluster rabbit@$JOIN_CLUSTER_HOST
   rabbitmqctl start_app
   echo "Cluster status:"
   rabbitmqctl cluster_status
 fi
 
+wait
+
 # Keep the container running
-exec rabbitmq-server
+#exec rabbitmq-server
