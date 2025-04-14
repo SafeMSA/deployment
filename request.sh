287b5e8b2317230b0afcb21b70aed73e4172c59c
martinaskolin <martinaskolin99@gmail.com>
2025-04-14 12:20:41 +0200

    hashd

--- request.sh
+++ request.sh
@@ -1,8 +1,8 @@
-for i in {1..1000}; do
+for i in {1..200}; do
   response=$(curl -s -X POST http://localhost:9091 \
     -H "Content-Type: application/json" \
     -d "{\"message\": \"Evacuate immediately!\"}")
   
-  echo "$response" | jq -r '"\(.id), \(.name)"'
-  sleep 1
+  echo "$response" | jq -r '"\(.id), \(.status)"'
+  sleep 5
 done
